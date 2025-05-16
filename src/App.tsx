2780ddf06f4bf9674aa8df566af5fa0cb2a89c9f
SimoneZampieri <simonezampieri106@gmail.com>
2025-05-16 11:47:52 +0200

    add logout button

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,8 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/LoginPage';
+import DashboardPage from './pages/DashboardPage';
+import ProtectedRoute from './components/ProtectedRoute';
 
 const App = () => {
   return (
@@ -11,6 +13,16 @@ const App = () => {
         <Routes>
           {/* Rotta per la pagina di login */}
           <Route path="/login" element={<LoginPage />} />
+          
+          {/* Rotta protetta per la dashboard */}
+          <Route 
+            path="/dashboard" 
+            element={
+              <ProtectedRoute>
+                <DashboardPage />
+              </ProtectedRoute>
+            } 
+          />
           
           {/* Reindirizzamento della rotta principale alla pagina di login */}
           <Route path="/" element={<Navigate to="/login" replace />} />
